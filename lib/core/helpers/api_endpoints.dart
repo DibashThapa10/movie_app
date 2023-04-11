bc5a4c18271c580a720cc5565acc40539e0296fe
dibashthapa10 <dibashthapa447@gmail.com>
2023-04-11 11:45:51 +0545

    handle response in movie repository

---
+++ lib/core/helpers/api_endpoints.dart
@@ -0,0 +1,3 @@
+class ApiEndPoints {
+  static const String getMoviesListUrl = "https://api.themoviedb.org/3/trending/movie/week?api_key=060e7c76aff06a20ca4a875981216f3f";
+}
---
+++ lib/features/home/data/models/movie_model.dart
@@ -0,0 +1,7 @@
+class MovieModel {
+  final String title;
+  final String image;
+  final String releaseDate;
+
+  MovieModel({required this.title, required this.image,required this.releaseDate});
+}
